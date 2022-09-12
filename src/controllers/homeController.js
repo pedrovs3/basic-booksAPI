d0af179b8f4d76515bd06a71958aef63e753ff9a
Pedro Vieira <pedrovs3@hotmail.com>
2022-09-12 11:10:17 -0300

    Update homeController.js

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -7,7 +7,6 @@ exports.index = async (req, res) => {
 };
 
 exports.indexQuery = async (req, res) => {
-  console.log(req.query.keyword);
   const livros = await books.jsonReturn(req.query.keyword);
   if (livros) res.status(200).json(livros);
   else res.status(404).json({ error: "Don't have books with this keyword." });
